af975534a0583a65a388647dba92d88adae1acee
Artifique <148628912+Artifique@users.noreply.github.com>
2024-07-22 19:43:10 +0200

    Add files via upload

--- FrontGestEvent/src/app/app.component.ts
+++ FrontGestEvent/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { CorpsComponent } from "./corps/corps.component";
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    CorpsComponent,
+    RouterModule
+    // RouterLink,
+    // RouterLinkActive,
+    ,
+    LoginComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'FrontGestEvent';
+  title = 'projet';
 }
--- FrontGestEvent/src/app/app.routes.ts
+++ FrontGestEvent/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CorpsComponent } from './corps/corps.component';
+import{EvenementComponent} from './evenement/evenement.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    // { path: '', component: AppComponent },
+    { path: 'evenement', component: NavbarComponent },
+    { path: 'toto', component: SidebarComponent },
+    { path: 'reservation', component: ReservationComponent }
+    
+    
+];
