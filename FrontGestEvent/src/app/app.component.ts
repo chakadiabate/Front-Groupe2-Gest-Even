6aad4c3847e44d54ef880ff624a1d846e631ae16
S0ul3y <maigasouleymane244@gmail.com>
2024-07-22 09:43:02 +0000

    22/07/2024 09:42 Maiga

---
+++ FrontGestEvent/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { CorpsComponent } from "./corps/corps.component";
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    CorpsComponent,
+    RouterModule
+    // RouterLink,
+    // RouterLinkActive,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'projet';
+}
